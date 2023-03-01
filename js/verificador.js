0ef4e2454ffda6a509a5d5ff8e7a084ef9b7b413
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2023-03-01 16:39:17 -0300

    Adicionando botão virar tudo

--- js/verificador.js
+++ js/verificador.js
@@ -1,11 +1,29 @@
 function teste (element) {
     
     let ocultar = document.getElementById(`${element.id}`)
-    console.log(ocultar);
 
     ocultar.classList.toggle('ativo');
     ocultar.classList.toggle('opacidade');
-
      
 }
 
+function virarTudo() {
+
+    let virar = document.querySelectorAll('.ativo');
+    let desvirar = document.querySelectorAll('.opacidade');
+
+    for (let i = 0; i < virar.length; i++) {
+        
+        virar[i].classList.toggle('ativo');
+        virar[i].classList.toggle('opacidade');
+    
+    }
+
+    for (let i = 0; i < desvirar.length; i++) {
+        
+        desvirar[i].classList.toggle('ativo');
+        desvirar[i].classList.toggle('opacidade');
+    
+    }
+
+}
