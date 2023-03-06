848675276d60ed6224c80bb51fccba24352658c2
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2023-03-06 19:21:52 -0300

    Melhorando a função de escolher imagem

--- js/escolha.js
+++ js/escolha.js
@@ -1,46 +1,97 @@
 let image = document.getElementById('imagem');
 let seletor = document.getElementById('seletor');
 
-function escolha() {
-    if(seletor.value === "Casimiro") {
-    image.src = 'img/cards/casimiro.JPG';
-    } else if(seletor.value === "Whindersson") {
-    image.src = 'img/cards/whindersson.JPG';
-    } else if(seletor.value === "Anitta") {
-    image.src = 'img/cards/anitta.JPG';
-    } else if(seletor.value === "Ronaldinho") {
-    image.src = 'img/cards/ronaldinho.JPG';
-    } else if(seletor.value === "Ludmilla") {
-    image.src = 'img/cards/ludmilla.JPG';
-    } else if(seletor.value === "Ivete Sangalo") {
-    image.src = 'img/cards/ivete.JPG';
-    } else if(seletor.value === "Iza") {
-    image.src = 'img/cards/iza.JPG';
-    } else if(seletor.value === "Larissa Manoela") {
-    image.src = 'img/cards/larissa_manoela.JPG';
-    } else if(seletor.value === "Luan Santana") {
-    image.src = 'img/cards/luan.JPG';
-    } else if(seletor.value === "Maisa") {
-    image.src = 'img/cards/maisa.JPG';
-    } else if(seletor.value === "Neymar") {
-    image.src = 'img/cards/neymar.JPG';
-    } else if(seletor.value === "Rodrigo Faro") {
-    image.src = 'img/cards/rodrigo.JPG';
-    } else if(seletor.value === "Silvio Santos") {
-    image.src = 'img/cards/silvio_santos.JPG';
-    } else if(seletor.value === "Thiaguinho") {
-    image.src = 'img/cards/thiaguinho.JPG';
-    } else if(seletor.value === "Tata Werneck") {
-    image.src = 'img/cards/tata.JPG';
-    } else if(seletor.value === "Tiago Leifert") {
-    image.src = 'img/cards/tiago_leifert.JPG';
-    } else if(seletor.value === "Danilo Gentilli") {
-    image.src = 'img/cards/danilo.JPG';
-    } else if(seletor.value === "Gisele Bündchen") {
-    image.src = 'img/cards/gisele.JPG';
-    } else if(seletor.value === "Marta") {
-    image.src = 'img/cards/marta.JPG';
-    } else if(seletor.value === "Bruna Marquezine") {
-    image.src = 'img/cards/brunamarquezine.JPG';
-    }  
+function escolhaCard() {
+
+    const imagens = [
+        {
+            nome: 'Anitta',
+            imagem: 'img/cards/anitta.JPG'
+        },
+        {
+            nome: 'Bruna Marquezine',
+            imagem: 'img/cards/brunamarquezine.JPG'
+        },
+        {
+            nome: 'Casimiro',
+            imagem: 'img/cards/casimiro.JPG'
+        },
+        {
+            nome: 'Danilo Gentilli',
+            imagem: 'img/cards/danilo.JPG'
+        },
+        {
+            nome: 'Gisele Bündchen',
+            imagem: 'img/cards/gisele.JPG'
+        },
+        {
+            nome: 'Ivete Sangalo',
+            imagem: 'img/cards/ivete.JPG'
+        },
+        {
+            nome: 'Iza',
+            imagem: 'img/cards/iza.JPG'
+        },
+        {
+            nome: 'Larissa Manoela',
+            imagem: 'img/cards/larissa_manoela.JPG'
+        },
+        {
+            nome: 'Luan Santana',
+            imagem: 'img/cards/luan.JPG'
+        },
+        {
+            nome: 'Ludmilla',
+            imagem: 'img/cards/ludmilla.JPG'
+        },
+        {
+            nome: 'Maisa',
+            imagem: 'img/cards/maisa.JPG'
+        },
+        {
+            nome: 'Marta',
+            imagem: 'img/cards/marta.JPG'
+        },
+        {
+            nome: 'Neymar',
+            imagem: 'img/cards/neymar.JPG'
+        },
+        {
+            nome: 'Rodrigo Faro',
+            imagem: 'img/cards/rodrigo.JPG'
+        },
+        {
+            nome: 'Ronaldinho',
+            imagem: 'img/cards/ronaldinho.JPG'
+        },
+        {
+            nome: 'Silvio Santos',
+            imagem: 'img/cards/silvio_santos.JPG'
+        },
+        {
+            nome: 'Tata Werneck',
+            imagem: 'img/cards/tata.JPG'
+        },
+        {
+            nome: 'Thiaguinho',
+            imagem: 'img/cards/thiaguinho.JPG'
+        },
+        {
+            nome: 'Tiago Leifert',
+            imagem: 'img/cards/tiago_leifert.JPG'
+        },
+        {
+            nome: 'Whindersson',
+            imagem: 'img/cards/whindersson.JPG'
+        }
+    ]
+
+    for (let card of imagens) {
+        
+        if(card.nome === seletor.value) {
+            image.src = card.imagem;
+        }
+    
+    }
+
 }
